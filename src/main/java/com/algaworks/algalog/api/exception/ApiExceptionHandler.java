0136bb3775c24d0bd06a8f48b3abff24beb468f3
Jose Lemes <jose.lemes7@gmail.com>
2023-05-19 14:28:45 -0300

    Response Entity Exception Handler

---
+++ src/main/java/com/algaworks/algalog/api/exception/ApiExceptionHandler.java
@@ -0,0 +1,42 @@
+package com.algaworks.algalog.api.exception;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+		
+		List<Problema.Campo> campos = new ArrayList<>();
+		
+		List<ObjectError> errors = ex.getBindingResult().getAllErrors();
+		errors.forEach(error -> {
+			FieldError field = (FieldError)error;
+			String nome = field.getField();
+			String mensagem = error.getDefaultMessage();
+			campos.add(new Problema.Campo(nome, mensagem));
+		});
+
+		Problema problema = new Problema();
+		problema.setStatus(status.value());
+		problema.setDataHora(LocalDateTime.now());
+		problema.setTitulo("Um ou mais campos estão inválidos");		
+		problema.setCampos(campos);
+		
+		return handleExceptionInternal(ex, problema, headers, status, request);
+	}
+}
